fec5a605107a78e63a142475f59f20f01ded4294
syah1404 <147252331+syah1404@users.noreply.github.com>
2024-03-12 21:21:28 +0700

    Add files via upload

---
+++ tugas4/4b.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4</title>
+</head>
+<body>
+
+    <h3>Macam-macam perangkat keras komputer</h3>
+
+    <ol>
+        
+        <?php 
+
+            $hardware = [
+                "Motherboard",
+                "Processor",
+                "Hard Disk",
+                "PC Coller",
+                "VGA Card",
+                "SSD"
+            ];
+
+            foreach ($hardware as $hdw) {
+                echo "<li>$hdw</li>";
+            }
+
+        ?>
+    </ol>
+
+    <h3>Macam-macam perangkat keras komputer baru</h3>
+
+    <ol>
+         <?php 
+         
+          array_push($hardware, "Card Reader", "Modem");
+
+          sort($hardware);
+
+          foreach ($hardware as $hdw) {
+            echo "<li>$hdw</li>";
+          }
+         
+         ?>   
+    </ol>
+    
+</body>
+</html>
